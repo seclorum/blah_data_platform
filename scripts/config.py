e935284461b5223fe07176d8a70236e7c3170d22
Jay Vaughan <seclorum@mac.com>
2025-01-20 19:36:38 +0100

    ../data is used in config for testing

--- scripts/config.py
+++ scripts/config.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Directory to store database and fetched data
-DATA_DIR = os.path.join(BASE_DIR, "data")
+DATA_DIR = os.path.join(BASE_DIR, "../data")
 
 # Database file path
 DB_FILE = os.path.join(DATA_DIR, "war_data.db")
